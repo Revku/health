53abc8b503a1e4aa92a3a253778b506b0c3d6f68
Patryk Skolimowski <revku@revku.dev>
2024-07-26 21:13:45 +0200

    chore: change author logo in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,5 @@
+import logo from "@/assets/revkudevlogo.svg";
+import Image from "next/image";
 import { Separator } from "./ui/separator";
 
 export default function Footer() {
@@ -10,12 +12,23 @@ export default function Footer() {
           Open source |{" "}
           <a
             href="https://github.com/Revku/health"
+            target="_blank"
             className="font-medium text-blue-500 transition-colors hover:text-blue-400"
           >
             Repozytorium GitHub
           </a>
         </p>
-        <p className="mt-[10px] text-sm">Wykonane przez Revku.dev</p>
+        <p className="mt-[10px] flex gap-2 text-sm">
+          <span>Wykonane przez</span>
+          <a
+            href="https://revku.dev"
+            target="_blank"
+            className="flex items-center justify-center gap-[5px]"
+          >
+            <Image src={logo} alt="Revku.dev Logo" className="w-[20px]" />
+            <span className="font-medium transition-colors hover:text-red-500">revku.dev</span>
+          </a>
+        </p>
       </div>
     </div>
   );
