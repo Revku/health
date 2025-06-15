4ed865e9f6f73f4a4014647978d4ad450f4b1756
Jakub Zaborowski <superbobek11@wp.pl>
2025-06-15 20:13:41 +0200

    feat: add auth context

---
+++ src/context/auth-context.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import { createContext, FC, ReactNode, useContext, useEffect, useState } from 'react';
+
+import { auth } from '@/utils/firebase';
+import { onAuthStateChanged, User, signInWithEmailAndPassword } from 'firebase/auth';
+
+type AuthContextType = {
+  user: User | null;
+  logIn: (email: string, password: string) => Promise<void>;
+  logOut: () => Promise<void>;
+};
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export const AuthProvider: FC<AuthProviderProps> = ({ children }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const logIn = async (email: string, password: string) => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      setUser(userCredential.user);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  const logOut = async () => {
+    await auth.signOut();
+  };
+
+  return (
+      <AuthContext.Provider value={{ user, logOut, logIn }}>{children}</AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const ctx = useContext(AuthContext);
+
+  if (!ctx) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+
+  return ctx;
+};
